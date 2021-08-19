2bbf64f054c817112964409359dca9f34242a5d6
M-Ribero <gm.mts1@gmail.com>
2021-08-19 03:16:17 -0300

    sdfssdfdfgdf
    sfdsd

--- js/login.js
+++ js/login.js
@@ -1,6 +1,23 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
 
 });
+
+
+
+
+function validacion() {
+    
+    let usuario = document.getElementById("correo").value;
+    let contrasena = document.getElementById("contrasena").value;
+
+    if (usuario !== "" && contrasena !== "") {
+        window.location.assign("index.html");
+       // windows.location = "index.html";
+    }
+    else {
+        document.getElementById("datos-error").innerHTML = "debe ingresar sus datos";
+    }
+}
