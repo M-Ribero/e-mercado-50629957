e730a68ff42f2875b24776c9076e1c65d4203477
M-Ribero <gm.mts1@gmail.com>
2021-10-14 12:30:54 -0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -6,24 +6,22 @@ const CARRITO_ALT = "https://japdevdep.github.io/ecommerce-api/cart/654.json";
 function showArticles(array) {
     contenido = "";
     for (let i = 0; i < array.length; i++) {
-            contenido += `
+        contenido += `
             <div class="col">
             <div class="mb-1">
             <h4>`+ array[i].name + `</h4>
             <img src="`+ array[i].imgSrc + `" alt=" " class="img-thumbnail" style="height: 200px"></img>
             <p> `+ array[i].unitCost + ' ' + array[i].currency + `</p>
-            <input type="number" value="`+ array[i].count +`" min="1" class="cantidadProducto"> 
+            <input style="width:50px;" id="cantidad${i}" type="number" value="`+ array[i].count + `" min="1" class="cantidadProducto"> 
             </div>
             </div>
         `
-}
-document.getElementById("carrito").innerHTML = contenido;
-
-// onchange="${calculoSubtotal()}"
+    }
+    document.getElementById("carrito").innerHTML = contenido;
 
 }
-
-function calculoTotal(){
+/*
+function calculoTotal() {
     let total = 0;
     let subTotal = document.getElementById("costoProductos");
 }
@@ -31,17 +29,19 @@ function calculoTotal(){
 
 function calculoSubtotal() {
     document.getElementById("costoProductos").innerHTML = "jaj";
-    }
+}*/
+
+function (){
+    console.log(document.getElementById(`cantidad${i}`).value);
+}
 
 
-    console.log(parseInt(document.getElementsByClassName("cantidadProducto").value));
 
 
 
 
 
-
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(CART_INFO_URL).then(function (resultObj) {
         if (resultObj.status === "ok") {
             articulos = resultObj.data;
