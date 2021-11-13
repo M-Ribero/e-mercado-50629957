5ee0fb9741b13239187081700da921c18b0aa2d7
M-Ribero <gm.mts1@gmail.com>
2021-11-13 01:47:12 -0300

    entrega 7 completa

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,6 @@
 var articulos = "";
 var articulosCargados = [];
+var envioActual = 0;
 const CARRITO_ALT = "https://japdevdep.github.io/ecommerce-api/cart/654.json";
 
 
@@ -21,7 +22,7 @@ function showArticles(array) {
             <img src="`+ articulo.src + `" alt=" " class="img-thumbnail" style="height: 200px"></img>
             <p id="precio${i}">` + articulo.unitCost + `</p>
             <p id="moneda${i}">` + articulo.currency + `</p>
-            <input style="width:50px;" onchange="calculoSubtotal(${i})" id="cantidad${i}" type="number" value="` + articulo.count + `" min="1" class="cantidadProducto">
+            <input style="width:50px;" onchange="calculoProductos(${i})" id="cantidad${i}" type="number" value="` + articulo.count + `" min="1" class="cantidadProducto">
             <p id="subtotalArticulo${i}">${sub}</p> 
             </div>
             </div>
@@ -29,27 +30,38 @@ function showArticles(array) {
     }
     document.getElementById("carrito").innerHTML = contenido;
     convertir("UYU", array.length);
-    calculoArticulos(array);
+    calculoTotal(array);
 }
 
 
-function calculoSubtotal(i) {
+function calculoProductos(i) {
     subtotal = 0;
     subtotal += document.getElementById(`precio${i}`).innerHTML * document.getElementById(`cantidad${i}`).value;
     document.getElementById(`subtotalArticulo${i}`).innerHTML = subtotal;
-    calculoArticulos(articulosCargados);
+    calculoTotal(articulosCargados);
+}
+
+function calculoEnvio(porcentaje) {
+    envioActual = porcentaje
+    calculoTotal(articulosCargados);
 }
 
 
-function calculoArticulos(articulos) {
+function calculoTotal(articulos) {
     document.getElementById("costoProductos").innerHTML = 0;
     total = 0;
+    envio = 0;
     for (let i = 0; i < articulos.length; i++) {
         total += document.getElementById(`precio${i}`).innerHTML * document.getElementById(`cantidad${i}`).value;
     }
     document.getElementById("costoProductos").innerHTML = total;
-    // costo total temporal sin calcular envío
-    document.getElementById("costoTotal").innerHTML = document.getElementById("costoProductos").innerHTML;
+
+    envio = total * envioActual / 100;
+    if (envio != 0) {
+        document.getElementById("costoEnvio").innerHTML = envio;
+    }
+
+    document.getElementById("costoTotal").innerHTML = total + envio;
 }
 
 
@@ -64,15 +76,43 @@ function convertir(tipoDeCambio, cantidadArticulos) {
             document.getElementById(`precio${i}`).innerHTML = document.getElementById(`precio${i}`).innerHTML / 40;
             document.getElementById(`moneda${i}`).innerHTML = "USD";
         }
-        calculoSubtotal(i);
+        calculoProductos(i);
     }
 }
 
 
-// VALIDACIONES
+// VALIDACIONES MODAL
 
+function seleccionarMetodo(metodo) {
+    if (metodo == 1) {
+        document.getElementById("numeroCuenta").disabled = true;
+        document.getElementById("numeroTarjeta").disabled = false;
+        document.getElementById("vencimiento").disabled = false;
+        document.getElementById("cvv").disabled = false;
+        document.getElementById("numeroCuenta").required = false;
+        document.getElementById("numeroTarjeta").required = true;
+        document.getElementById("vencimiento").required = true;
+        document.getElementById("cvv").required = true;
+    }
+    else {
+        document.getElementById("numeroCuenta").disabled = false;
+        document.getElementById("numeroTarjeta").disabled = true;
+        document.getElementById("vencimiento").disabled = true;
+        document.getElementById("cvv").disabled = true;
+        document.getElementById("numeroCuenta").required = true;
+        document.getElementById("numeroTarjeta").required = false;
+        document.getElementById("vencimiento").required = false;
+        document.getElementById("cvv").required = false;
+    }
+}
 
-
+function advertenciaPago() {
+    if (document.getElementById("pagoTarjetaCredito").checked == false && document.getElementById("pagoTransferenciaBancaria").checked == false) {
+        document.getElementById("botonPago").classList.remove("btn-outline-success");
+        document.getElementById("botonPago").classList.add("btn-danger");
+    }
+    
+}
 
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(CARRITO_ALT).then(function (resultObj) {
